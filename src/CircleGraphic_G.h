d5e8e616d076406bc8e276d0fcb74550f91556b0
JACK <jkear@seas.upenn.edu>
2015-12-02 15:34:02 -0500

    added 'g' and 'a'

---
+++ src/CircleGraphic_G.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ofMain.h"
+#include "BaseGraphic.h"
+
+class CircleGraphic_G : public BaseGraphic {
+    float radius = 0;
+    int x = 0;
+    int y = 0;
+    float counter = 0;
+    int color = rand() % 55;
+    
+public:
+    void update() {
+        if (radius >= 1500) {
+            isDone = true;
+            ofBackground(255 - color * 2,255 - color * .5,255 - color);
+        }
+        
+        radius += 20;
+    }
+    void draw() {
+        
+        ofSetColor(255- color * 2,255 - color * .5,255 - color);
+        ofFill();
+        ofDrawCircle(x, y, radius);
+    }
+};
---
+++ src/CirclesGraphic_A.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "ofMain.h"
+#include "BaseGraphic.h"
+
+class CirclesGraphic_A : public BaseGraphic {
+    int x = 500;
+    int y = 400;
+    int w = 300;
+    int radius = 20;
+public:
+    void update() {
+        if (w <= 0){
+            isDone = true;
+        } else {
+            w -= 5;
+        }
+    }
+    void draw() {
+        
+        
+        ofSetColor(0,255,255);
+        ofFill();
+        
+        ofDrawCircle(x - w, y,radius);
+        ofDrawCircle(x + w,y,radius);
+        ofDrawCircle(x ,y + w,radius);
+        ofDrawCircle(x ,y - w,radius);
+        
+    }
+};
+
