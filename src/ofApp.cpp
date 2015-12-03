195b77274d407843dfa976351a0ef8f8a1b1f5be
JACK <jkear@seas.upenn.edu>
2015-12-03 17:12:00 -0500

    fixed: accidentally uploaded empty version of ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,22 +2,31 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+       ofSetCircleResolution(50);
+       ofBackground(255,255,255);
+       ofSetWindowTitle("Ratatat");
+       ofSetFrameRate(60); // if vertical sync is off, we can go a bit fast... this caps the framerate at 60fps.
+       sHandler.setup();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+      gHandler.update();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+     gHandler.draw();
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+        gHandler.addGraphic(key);
+       sHandler.play(key);
+    
 }
 
 //--------------------------------------------------------------
